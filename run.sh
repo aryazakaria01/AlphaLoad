cfe0ce721f562c0ec6ce6302ac5793125acffb5b
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-07-28 22:39:08 +0700

    Fixed

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 echo "Cloning the repo..."
 # clone the repo
-git clone https://github.com/TeamUltroid/Ultroid.git /root/TeamUltroid/
+git clone https://github.com/AftahBagas/Alpha-Userbot.git /root/AftahBagas/
 
 echo "Cloned Successfully."
